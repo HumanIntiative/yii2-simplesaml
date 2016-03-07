09208a1b79904b63d8c7a5a08d61315e9358b6e3
Zein Miftah <zeinmiftah@gmail.com>
2016-03-07 14:21:44 +0700

    Init commit

---
+++ UserIdentity.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace itdev\simplesaml;
+
+use Yii;
+
+class UserIdentity extends \yii\base\Object implements \yii\web\IdentityInterface
+{
+    public function __get($name)
+    {
+        $user = Yii::$app->getUser();
+        if ($user->hasAttribute($name))
+            return $user->getAttribute($name);
+        else
+            parent::__get($name);
+    }
+
+    /**
+     * Finds an identity by the given ID.
+     * @param string|integer $id the ID to be looked for
+     * @return IdentityInterface the identity object that matches the given ID.
+     * Null should be returned if such an identity cannot be found
+     * or the identity is not in an active state (disabled, deleted, etc.)
+     */
+    public static function findIdentity($id)
+    {
+        return new self();
+    }
+
+    /**
+     * Finds an identity by the given token.
+     * @param mixed $token the token to be looked for
+     * @param mixed $type the type of the token. The value of this parameter depends on the implementation.
+     * For example, [[\yii\filters\auth\HttpBearerAuth]] will set this parameter to be `yii\filters\auth\HttpBearerAuth`.
+     * @return IdentityInterface the identity object that matches the given token.
+     * Null should be returned if such an identity cannot be found
+     * or the identity is not in an active state (disabled, deleted, etc.)
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return new self();
+    }
+
+    /**
+     * Returns an ID that can uniquely identify a user identity.
+     * @return string|integer an ID that uniquely identifies a user identity.
+     */
+    public function getId()
+    {
+        return Yii::$app->getUser()->getAttribute('id');
+    }
+
+    /**
+     * Returns a key that can be used to check the validity of a given identity ID.
+     *
+     * The key should be unique for each individual user, and should be persistent
+     * so that it can be used to check the validity of the user identity.
+     *
+     * The space of such keys should be big enough to defeat potential identity attacks.
+     *
+     * This is required if [[User::enableAutoLogin]] is enabled.
+     * @return string a key that is used to check the validity of a given identity ID.
+     * @see validateAuthKey()
+     */
+    public function getAuthKey()
+    {
+        return null;
+    }
+
+    /**
+     * Validates the given auth key.
+     *
+     * This is required if [[User::enableAutoLogin]] is enabled.
+     * @param string $authKey the given auth key
+     * @return boolean whether the given auth key is valid.
+     * @see getAuthKey()
+     */
+    public function validateAuthKey($authKey)
+    {
+        return !Yii::$app->getUser()->getIsGuest();
+    }
+}
---
+++ WebUser.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace itdev\simplesaml;
+
+use Yii;
+use yii\web\User;
+use yii\base\InvalidConfigException;
+
+class WebUser extends User
+{
+    public $autoloaderPath;
+    public $authSource;
+    public $attributesConfig=[];
+
+    private $_saml;
+    private $_attributes;
+
+    public function getSaml()
+    {
+        if (!isset($this->_saml)) {
+
+            if (!isset($this->autoloaderPath))
+                throw new InvalidConfigException("SimpleSAMLPHP's autoloader file is not set");
+            if (!file_exists($this->autoloaderPath))
+                throw new InvalidConfigException("SimpleSAMLPHP's autoloader file doesn't exists");
+            if (!isset($this->authSource))
+                throw new InvalidConfigException("SimpleSAMLPHP's auth source is not set");
+            
+            require_once($this->autoloaderPath);
+
+            $this->_saml = new \SimpleSAML_Auth_Simple($this->authSource);
+
+            $this->checkId();
+        }
+
+        return $this->_saml;
+    }
+
+    protected function getAttributesInternal()
+    {
+        $saml = $this->getSaml();
+
+        $attributes = array();
+        $attributesConfig = $this->attributesConfig;
+        $coreAttributes = $saml->getAttributes();
+        
+        foreach ($attributesConfig as $localAttr => $coreAttr)
+            if (isset($coreAttributes[$coreAttr][0]))
+                $attributes[$localAttr] = $coreAttributes[$coreAttr][0];
+
+        return $attributes;
+    }
+
+    protected function setAttributes($attributes=array())
+    {
+        if (empty($attributes))
+            $this->_attributes = $this->getAttributesInternal();
+        else
+            $this->_attributes = $attributes;
+    }
+
+    public function getAttributes()
+    {
+        if (!isset($this->_attributes))
+            $this->setAttributes();
+
+        return $this->_attributes;
+    }
+
+    public function getAttribute($name)
+    {
+        if (!isset($this->_attributes))
+            $this->setAttributes();
+
+        if (isset($this->_attributes[$name]))
+            return $this->_attributes[$name];
+        else
+            return null;
+    }
+
+    public function hasAttribute($name)
+    {
+        if (!isset($this->_attributes))
+            $this->setAttributes();
+
+        if (isset($this->_attributes[$name]))
+            return true;
+        else
+            return false;
+    }
+
+    public function getIdentity($autoRenew = true)
+    {
+        $this->checkId();
+        return parent::getIdentity($autoRenew);
+    }
+
+    public function getIsGuest()
+    {
+        return !$this->getSaml()->isAuthenticated();
+    }
+
+    public function getId()
+    {
+        return $this->getAttribute('id');
+    }
+
+    /**
+     * @var string the class name of the [[identity]] object.
+     */
+    public $identityClass = 'itdev\simplesaml\UserIdentity';
+
+    /**
+     * Logs out the current user.
+     * This will remove authentication-related session data.
+     * If `$destroySession` is true, all session data will be removed.
+     * @param boolean $destroySession whether to destroy the whole session. Defaults to true.
+     * This parameter is ignored if [[enableSession]] is false.
+     * @return boolean whether the user is logged out
+     */
+    public function logout($destroySession = true)
+    {
+        if ($this->getSaml()->isAuthenticated()) {
+            $request = Yii::$app->getRequest();
+            $id = $this->getId();
+            $ip = $request->getUserIP();
+            Yii::info("User '$id' logging out from $ip by SimpleSAMLPHP.", __METHOD__);
+            $this->getSaml()->logout([
+                'ReturnTo' => $request->getUrl(),
+            ]);
+        }
+        if ($destroySession && $this->enableSession) {
+            Yii::$app->getSession()->destroy();
+        }
+
+        return $this->getIsGuest();
+    }
+
+    public function loginRequired($checkAjax = true)
+    {
+        $request = Yii::$app->getRequest();
+        if ($this->enableSession && (!$checkAjax || !$request->getIsAjax())) {
+            $this->setReturnUrl($request->getUrl());
+        }
+        if (!$this->getSaml()->isAuthenticated()) {
+            if ($checkAjax && $request->getIsAjax()) {
+                return Yii::$app->getResponse()->redirect($this->getReturnUrl());
+            } else {
+                $this->getSaml()->login([
+                    'ReturnTo' => $request->getUrl(),
+                    'KeepPost' => true,
+                ]);
+            }
+        }
+        $this->checkId(false);
+    }
+
+    protected function checkId($loginRequired = true, $checkAjax = true)
+    {
+        $id = $this->getId();
+        $session = Yii::$app->getSession();
+        if ($id !== null) {
+            if ($session->has($this->idParam)) {
+                if ($session->get($this->idParam)!==$id)
+                    $session->set($this->idParam, $id);
+            } else
+                $session->set($this->idParam, $id);
+        } elseif ($session->has($this->idParam) && $loginRequired)
+            $this->loginRequired($checkAjax);
+    }
+}
